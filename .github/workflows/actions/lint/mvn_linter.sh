dde1ae0b30e2c1066f6b86b2c8ea7f17e462f7de
ChidoW <warambwac@gmail.com>
2024-02-13 08:50:27 +0200

    update linte

--- .github/workflows/actions/lint/mvn_linter.sh
+++ .github/workflows/actions/lint/mvn_linter.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 set -eo pipefail
-sdfsdf = 97
+
 cd "$1"
 
+sdfsdf = 97
+
 for dir in */; do
-  dir="${dir%/}"  # Remove trailing slash
+  dir="${dir%/}" 
   if [ -f "$dir/pom.xml" ] "$dir"; then
     echo "Running Checkstyle for $dir ..."
     mvn checkstyle:check  -Dcheckstyle.config.location=$dir/checkstyle/suppression.xml
