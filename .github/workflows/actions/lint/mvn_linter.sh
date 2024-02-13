475824c5469f0a8767945b8b03ed1365345e5849
ChidoW <warambwac@gmail.com>
2024-02-13 09:14:02 +0200

    updating the mvn linter

--- .github/workflows/actions/lint/mvn_linter.sh
+++ .github/workflows/actions/lint/mvn_linter.sh
@@ -4,7 +4,7 @@ cd "$1"
 
 for dir in */; do
   dir="${dir%/}" 
-  if [ -f "$dir/pom.xml" ] "$dir"; then
+  if [ -f "$dir/pom.xml" ]; then
     echo "Running Checkstyle for $dir ..."
     mvn checkstyle:check  -Dcheckstyle.config.location=$dir/checkstyle/suppression.xml
   fi
