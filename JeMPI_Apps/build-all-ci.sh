5a416eb231fb91495a4f6253e335fa97a49ff786
ChidoW <warambwac@gmail.com>
2024-02-14 10:33:45 +0200

    using global build script

---
+++ JeMPI_Apps/build-all-ci.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set -u
+
+cp -L -f ./JeMPI_Configuration/config-api.json ./JeMPI_API/src/main/resources/config-api.json
+cp -L -f ./JeMPI_Configuration/config-api.json ./JeMPI_API_KC/src/main/resources/config-api.json
+
+mvn clean package
+pushd JeMPI_EM_Scala
+  sbt clean assembly
+popd
+
+pushd JeMPI_AsyncReceiver
+  ./build.sh || exit 1
+popd
+pushd JeMPI_ETL
+  ./build.sh || exit 1
+popd
+pushd JeMPI_Controller
+  ./build.sh || exit 1
+popd
+pushd JeMPI_EM_Scala
+  ./build.sh || exit 1
+popd
+pushd JeMPI_Linker
+  ./build.sh || exit 1
+popd
+pushd JeMPI_API
+  ./build.sh || exit 1
+popd
+pushd JeMPI_API_KC
+  ./build.sh || exit 1
+popd
+pushd JeMPI_Bootstrapper
+  ./build.sh || exit 1
+popd
+pushd JeMPI_UI
+  ./build-image.sh || exit 1
+popd
