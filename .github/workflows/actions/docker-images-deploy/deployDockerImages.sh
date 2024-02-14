48afe74a67a0d3709f81b6d0f4dce1d5fc0830f3
ChidoW <warambwac@gmail.com>
2024-02-14 21:48:48 +0200

    updated workflow logic

---
+++ .github/workflows/actions/docker-images-deploy/deployDockerImages.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+original_tag=$1
+push_tag=$2
+registry_url=$3
+username=$4
+password=$5
+
+if [ -z "$registry_url" ] || [ -z "$username" ] || [ -z "$password" ]; then
+    echo "Docker host details not set. Cannot push"
+    exit 1
+if
+
+
+if [ -z "$push_tag" ]; then
+    push_tag=$original_tag
+if
+
+docker login "$registry_url" -u "$username" -p "$password"
+
+IMAGE_LIST=$(docker image ls --filter "reference=*:$original_tag" --format "{{.Repository}}:{{.Tag}}")
+
+for IMAGE in $IMAGE_LIST; do
+    IFS=':' read -a image_details <<< "$IMAGE"
+    push_tag_url="$registry_url/$username/${image_details[0]}:$push_tag"
+
+    echo "Pushing image: $IMAGE to '$push_tag_url'"
+
+    docker tag "$IMAGE" $push_tag_url
+    docker push $push_tag_url
+done
