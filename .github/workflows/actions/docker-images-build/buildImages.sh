2a894c6921f85b7055af90a0c3938f80448e821b
ChidoW <warambwac@gmail.com>
2024-02-14 09:25:12 +0200

    updates to build process

--- .github/workflows/actions/docker-images-build/buildImages.sh
+++ .github/workflows/actions/docker-images-build/buildImages.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+current_directory=$(dirname "$0")
+source $current_directory/../../../../devops/linux/docker/conf/env/create-env-linux-high-1.sh
+source $current_directory/../../../../devops/linux/docker/conf/env/conf.env
+
 cd "$1"
 
 for dir in */; do
   dir="${dir%/}" 
-  if [ -f "$dir/docker" ] && [ -f "$dir/build.sh" ]; then
-    echo "Building docker images for $dir (tag: $3)"
-    ./$dir/build.sh
+  if [ "$(basename "$dir")" = "JeMPI_UI" ]; then
+    echo "Building docker images for ui: $dir (tag: $2)"
+    pushd $dir
+      ./build-image.sh || exit 1
+    popd
+  elif [ -d "$dir/docker" ] && [ -f "$dir/build.sh" ]; then
+    echo "Building docker images for $dir (tag: $2)"
+    pushd $dir
+      ./build.sh || exit 1
+    popd
   fi
 done
