b3b05d23db5af602669e748fe589521f193efa96
ChidoW <warambwac@gmail.com>
2024-02-14 09:37:35 +0200

    improvement

--- .github/workflows/actions/docker-images-build/buildImages.sh
+++ .github/workflows/actions/docker-images-build/buildImages.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-current_directory=$(dirname "$0")
-source $current_directory/../../../../devops/linux/docker/conf/env/create-env-linux-high-1.sh
-source $current_directory/../../../../devops/linux/docker/conf/env/conf.env
-
 cd "$1"
 
 for dir in */; do
